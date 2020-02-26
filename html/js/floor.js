ebdfefdb82c24cc3c056b768c9dd01baa121591c
justClouds <ron@moerman.online>
2020-02-26 20:20:12 +0100

    Fix rounding of numbers in Matrix Home panel

--- html/js/floor.js
+++ html/js/floor.js
@@ -1,5 +1,13 @@
 angular.module("floor", []).filter("floor", function() {
   return function(input) {
-    return  Math.floor(input);
+    if (input < 0) {
+      result = -(Math.floor(Math.abs(input)));
+      if (input < 0 && result == 0)
+        return "-0";
+      else
+        return "" + result;
+    }
+    else
+      return "" + Math.floor(input);
   };
 });
