6822cb509346f758657a5e089c6dfecb2d2325d4
justClouds <ron@moerman.online>
2020-02-27 21:04:43 +0100

    Change gas hourly usage to 2 decimal places

--- automation/jsr223/python/personal/gas_usage.py
+++ automation/jsr223/python/personal/gas_usage.py
@@ -29,26 +29,26 @@ def gasUsage(event):
 
     # Calculate consumption last hour
     used = float(str(PersistenceExtensions.deltaSince(ir.getItem(event.itemName), DateTime.now().minusHours(1))))
-    gasUsage.log.info("Last Hours Gas Usage [{0:.3f} m3]".format(used/1000))
     events.postUpdate("Gas_Use_Hour", "{0:.3f}".format(used))
+    gasUsage.log.info("Last Hour Gas Usage [{0:.3f} m3]".format(used/1000))
     cost = used * GAS_PRICE_PER_DM3
     events.postUpdate("Gas_Use_Hour_Cost", "{0:.2f}".format(cost))
-    summary = u"{0:.3f} m3, € {1:.2f}".format(used/1000, cost)
-    gasUsage.log.info(u"Gas Use Hour Summary [{}]".format(summary))
+    summary = u"{0:.2f} m3, € {1:.2f}".format(used/1000, cost)
     events.postUpdate("Gas_Use_Hour_Summary", summary)
+    gasUsage.log.info(u"Gas Use Hour Summary [{}]".format(summary))
 
     # Calculate consumption today
     used = float(str(PersistenceExtensions.deltaSince(ir.getItem(event.itemName), DateTime.now().withTimeAtStartOfDay())))
-    gasUsage.log.info("Today's Gas Ussage [{0:.3f}]".format(used/1000))
     events.postUpdate("Gas_Use_Day", "{0:.3f}".format(used))
+    gasUsage.log.info("Today's Gas Ussage [{0:.3f}]".format(used/1000))
     cost = used * GAS_PRICE_PER_DM3
     events.postUpdate("Gas_Use_Day_Cost", "{0:.2f}".format(cost))
     events.postUpdate("Gas_Use_Day_Summary", u"{0:.1f} m3, € {1:.2f}".format(used/1000, cost))
 
     # Calculate consumption this month
     used = float(str(PersistenceExtensions.deltaSince(ir.getItem(event.itemName), DateTime.now().withTimeAtStartOfDay().withDayOfMonth(1))))
-    gasUsage.log.info("This Month's Gas Usage [{0:.3f}]".format(used/1000))
     events.postUpdate("Gas_Use_Month", "{0:.3f}".format(used))
+    gasUsage.log.info("This Month's Gas Usage [{0:.3f}]".format(used/1000))
     cost = used * GAS_PRICE_PER_DM3
     events.postUpdate("Gas_Use_Month_Cost", "{0:.2f}".format(cost))
     events.postUpdate("Gas_Use_Month_Summary", u"{0:.0f} m3, € {1:.2f}".format(used/1000, cost))
@@ -58,8 +58,8 @@ def gasUsage(event):
     if used is None:
         used = float(str(event.itemState)) - 7374000
         gasUsage.log.info("Fix for missing year data, pre-fill [{}]".format(used))
-    gasUsage.log.info("This Contract Year's Gas Usage [{0:.3f}]".format(used/1000))
     events.postUpdate("Gas_Use_Year", str(used))
+    gasUsage.log.info("This Contract Year's Gas Usage [{0:.3f}]".format(used/1000))
     cost = used * GAS_PRICE_PER_DM3
     events.postUpdate("Gas_Use_Year_Cost", str(cost))
     events.postUpdate("Gas_Use_Year_Summary", u"{0:.0f} m3, € {1:.2f}".format(used/1000, cost))
