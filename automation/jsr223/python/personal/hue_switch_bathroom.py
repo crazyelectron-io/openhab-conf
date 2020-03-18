89089dd19b3201ba27c90363343e9def0dea3af8
Ron Moerman <ron@crazyelectron.io>
2020-03-18 14:25:50 +0100

    Add buienradar binding

--- automation/jsr223/python/personal/hue_switch_bathroom.py
+++ automation/jsr223/python/personal/hue_switch_bathroom.py
@@ -8,12 +8,10 @@ Changelog:
 ----------------------------------------------------------------------------------------------------
 '''
 
+
 from core.rules import rule
 from core.triggers import when
-# from core.log import logging, LOG_PREFIX
-# import configuration
-# reload(configuration)
-# from configuration import LOG_PREFIX
+
 
 # #===================================================================================================
 # @rule("Hue Switch Event1000", description="Hue Dimmer Switch Key 1 (ON) Initial Pressed", tags=["lights","switch"])
@@ -22,12 +20,14 @@ from core.triggers import when
 #     hueSwitchEvent1000.log.info("Event [{}] received".format(event.event))
 #     switch = str(event.channel).split(":")[3].split("_")[1]
 
+
 # #===================================================================================================
 # @rule("Hue Switch Event1001", description="Hue Dimmer Switch Key 1 (ON) Hold", tags=["lights","switch"])
 # @when("Channel hue:0820:0017882ec5b3:dim_bathroom:dimmer_switch_event triggered 1001.0")
 # def hueSwitchEvent1001(event):
 #     hueSwitchEvent1001.log.info("Event [{}] received".format(event.event))
 #     switch = str(event.channel).split(":")[3].split("_")[1]
+
 
 #===================================================================================================
 @rule("Hue Switch Event1002", description="Hue Dimmer Switch Key 1 (ON) Short Released", tags=["lights","switch"])
@@ -48,6 +48,7 @@ def hueSwitchEvent1002(event):
         # hueSwitchEvent1002.log.info("Fourth Button 1 (ON) Short Release state - reset to EVENING scene")
         events.sendCommand("Light_Scene_Bathroom", "EVENING")
 
+
 # #===================================================================================================
 # @rule("Hue Switch Event1003", description="Hue Dimmer Switch Key 1 (ON) Long Released", tags=["lights","switch"])
 # @when("Channel hue:0820:0017882ec5b3:dim_bathroom:dimmer_switch_event triggered 1003.0")
@@ -63,6 +64,7 @@ def hueSwitchEvent1002(event):
 #     hueSwitchEvent2000.log.info("Event [{}] received".format(event.event))
 #     switch = str(event.channel).split(":")[3].split("_")[1]
 
+
 #===================================================================================================
 @rule("Hue Switch Event2001.2002", description="Hue Dimmer Switch Key 2 (INCREASE) Hold", tags=["lights","switch"])
 @when("Channel hue:0820:0017882ec5b3:dim_bathroom:dimmer_switch_event triggered 2001.0")
@@ -73,6 +75,7 @@ def hueSwitchEvent20012(event):
     if items["Light_Scene_Bathroom"] != StringType("OFF"):
         events.sendCommand("gLight_Brightness_Bathroom", "INCREASE")
 
+
 # #===================================================================================================
 # @rule("Hue Switch Event2003", description="Hue Dimmer Switch Key 2 (INCREASE) Long Released", tags=["lights","switch"])
 # @when("Channel hue:0820:0017882ec5b3:dim_bathroom:dimmer_switch_event triggered 2003.0")
@@ -80,12 +83,14 @@ def hueSwitchEvent20012(event):
 #     hueSwitchEvent2003.log.info("Event [{}] received".format(event.event))
 #     switch = str(event.channel).split(":")[3].split("_")[1]
 
+
 # #===================================================================================================
 # @rule("Hue Switch Event 3000", description="Hue Dimmer Switch Key 3 (DECREASE) Initial Pressed", tags=["lights","switch"])
 # @when("Channel hue:0820:0017882ec5b3:dim_bathroom:dimmer_switch_event triggered 3000.0")
 # def hueSwitchEvent3000(event):
 #     hueSwitchEvent3000.log.info("Event [{}] received".format(event.event))
 #     switch = str(event.channel).split(":")[3].split("_")[1]
+
 
 #===================================================================================================
 @rule("Hue Switch Event3001.3002", description="Hue dimmer Key 3 (DECREASE)", tags=["lights","switch"])
@@ -97,6 +102,7 @@ def hueSwitchEvent30012(event):
     if items["Light_Scene_Bathroom"] != StringType("OFF"):
         events.sendCommand("gLight_Brightness_Bathroom", "DECREASE")
 
+
 # #===================================================================================================
 # @rule("Hue Switch Event3003", description="Hue Dimmer Switch Key 3 (DECREASE) Long Released", tags=["lights","switch"])
 # @when("Channel hue:0820:0017882ec5b3:dim_bathroom:dimmer_switch_event triggered 3003.0")
@@ -104,12 +110,14 @@ def hueSwitchEvent30012(event):
 #     hueSwitchEvent3003.log.info("Event [{}] received".format(event.event))
 #     switch = str(event.channel).split(":")[3].split("_")[1]
 
+
 # #===================================================================================================
 # @rule("Hue Switch Event4000", description="Hue Dimmer Switch Key 4 (OFF) Initial Pressed", tags=["lights","switch"])
 # @when("Channel hue:0820:0017882ec5b3:dim_bathroom:dimmer_switch_event triggered 4000.0")
 # def hueSwitchEvent4000(event):
 #     hueSwitchEvent4000.log.info("Event [{}] received".format(event.event))
 #     switch = str(event.channel).split(":")[3].split("_")[1]
+
 
 #===================================================================================================
 @rule("Hue Switch Event4001", description="Hue Dimmer Switch Key 4 (OFF) Hold", tags=["lights","switch"])
@@ -119,6 +127,7 @@ def hueSwitchEvent4001(event):
     # switch = str(event.channel).split(":")[3].split("_")[1]
     events.sendCommand("Light_Scene_Bathroom", "OFF")
 
+
 #===================================================================================================
 @rule("Hue Switch Event4002", description="Hue Dimmer SwitchKey 4 (OFF) Short Released", tags=["lights","switch"])
 @when("Channel hue:0820:0017882ec5b3:dim_bathroom:dimmer_switch_event triggered 4002.0")
@@ -127,6 +136,7 @@ def hueSwitchEvent4002(event):
     # switch = str(event.channel).split(":")[3].split("_")[1]
     if items["Light_Scene_Bathroom"] != StringType("OFF"):
         events.sendCommand("Light_Scene_Bathroom", "OFF")
+
 
 #===================================================================================================
 @rule("Hue Switch Event4003", description="Hue Dimmer Switch Key 4 (OFF) Long Released", tags=["lights","switch"])
