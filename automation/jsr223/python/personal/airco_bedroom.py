c56148a22bf9f8d95c959005a077ff71adb41d49
Ron Moerman <ron@crazyelectron.io>
2020-08-19 20:53:44 +0200

    Changed logging level for airco bedroom rule

--- automation/jsr223/python/personal/airco_bedroom.py
+++ automation/jsr223/python/personal/airco_bedroom.py
@@ -18,25 +18,24 @@ from core.utils import iround
 
 
 #===================================================================================================
-# @when("Item AC_Temp_Bedroom changed")
 @rule("AC Bedroom Handling", description="Turn on or off AC in Bedroom based on time and temperature", tags=["astro"])
 @when("Item Alarm_Door_Bedroom changed")
-@when("Time cron 0 */15 * * * ?")
+@when("Time cron 0 */10 * * * ?")
 @when("System started")
 def aircoBedroom(event):
-    # Define threshold values
+    #--- Define threshold values
     tempNightThreshold = 18
     tempMorningThreshold = 19
     tempDayThreshold = 20
     tempThreshold = tempDayThreshold
     outsideTempHiThreshold = 23
     outsideTempLoThreshold = 16
-    # Get current temperature readings
+    #--- Get current temperature readings
     tempBedroom = iround(float(str(items["AC_Temp_Bedroom"])))
     tempOutside = iround(float(str(items["AC_Temp_Outdoor"])))
-    # Set status variables to default state
+    #--- Set status variables to default state
     doorIsOpen = False
-    # Set default state of the action variables
+    #--- Set default state of the action variables
     turnOn = 0
     msg = ""
     aircoBedroom.log.info(">>> Enter rule with inside temp [{}] and outside temp [{}] >>>".format(tempBedroom, tempOutside))
@@ -45,9 +44,9 @@ def aircoBedroom(event):
         aircoBedroom.log.info("00. Vacation mode is on, exit Daikin rule")
         return
 
-    # Check the current airco on/off status
+    #--- Check the current airco on/off status
     if not isinstance(ir.getItem("AC_Power_Bedroom"), UnDefType) and items["AC_Power_Bedroom"] == ON:
-        aircoBedroom.log.info("01. Airco is currently ON")
+        aircoBedroom.log.debug("01. Airco is currently ON")
     # Check if the door is open
     if not isinstance(ir.getItem("Alarm_Door_Bedroom"), UnDefType) and items["Alarm_Door_Bedroom"] == OPEN:
         doorIsOpen = True
@@ -60,34 +59,34 @@ def aircoBedroom(event):
         tempThreshold = tempMorningThreshold
     else:
         tempThreshold = tempDayThreshold
-    aircoBedroom.log.info("03. Inside temp threshold of [{}] used".format(tempThreshold))
+    aircoBedroom.log.debug("03. Inside temp threshold of [{}] used".format(tempThreshold))
     # Turn on the airco if it is very hot outside, regardless of time of day or bedroom temperature 
     if tempOutside > outsideTempHiThreshold:
         if items["AC_Power_Bedroom"] != ON:
             msg = "It is hot outside, so I turned on the airco in the bedroom for you to keep it cool"
             turnOn = 1
-        aircoBedroom.log.info("04. Outside temp above [{}], set turn-AC-on flag [{}]".format(tempOutside, turnOn))
+        aircoBedroom.log.debug("04. Outside temp above [{}], set turn-AC-on flag [{}]".format(tempOutside, turnOn))
 
     # Check if bedroom temperature is above threshold to turn AC on
     if tempBedroom > tempThreshold:
         msg = "The bedroom temperature is rising, so I turned on the airco to keep it cool"
         turnOn = 1
-        aircoBedroom.log.info("06. Bedroom temp [{}] > [{}], turn-AC-on flag is set to [{}]".format(tempBedroom, tempThreshold, turnOn))
+        aircoBedroom.log.debug("06. Bedroom temp [{}] > [{}], turn-AC-on flag is set to [{}]".format(tempBedroom, tempThreshold, turnOn))
     else:
         # If bedroom is cooled off, check if outside is cool as well
         if tempOutside < outsideTempLoThreshold:
             msg = "The bedroom is cool now, so I turned off the airco for you"
             turnOn = -1
-            aircoBedroom.log.info("07. Bedroom temp is [{}], turn-AC-on flag set to [{}]".format(tempBedroom, turnOn))
+            aircoBedroom.log.debug("07. Bedroom temp is [{}], turn-AC-on flag set to [{}]".format(tempBedroom, turnOn))
         else:
-            aircoBedroom.log.info("08. Bedroom temp [{}] below threshold, but outside temp [{}] above threshold, not turning off the AC if still on, AC status is [{}]".format(tempBedroom, tempOutside, items["AC_Power_Bedroom"] == ON))
+            aircoBedroom.log.debug("08. Bedroom temp [{}] below threshold, but outside temp [{}] above threshold, not turning off the AC if still on, AC status is [{}]".format(tempBedroom, tempOutside, items["AC_Power_Bedroom"] == ON))
 
-    # Turn off the Airco if the door is open
+    #--- Turn off the Airco if the door is open
     if doorIsOpen:
         if turnOn == 1:
             msg = "The bedroom temperature is rising, but I cannot turn on the airco because the door is open. Please close the door first."
             turnOn = -1
-            aircoBedroom.log.info("09. The door is open and Turn-AC-On flag is set")
+            aircoBedroom.log.debug("09. The door is open and Turn-AC-On flag is set")
         if items["AC_Power_Bedroom"] == ON:
             events.sendCommand("AC_Power_Bedroom", "OFF")
             turnOn = 0
@@ -106,25 +105,25 @@ def aircoBedroom(event):
                 events.sendCommand("AC_FanSpeed_Bedroom", "SILENT")
         else:
             msg = ""
-            aircoBedroom.log.info("12. AC already on, ignoring turn on action")
+            aircoBedroom.log.debug("12. AC already on, ignoring turn on action")
     elif turnOn == -1:
         if items["AC_Power_Bedroom"] == ON:
             aircoBedroom.log.info("13. Turn off the AC")
             events.sendCommand("AC_Power_Bedroom", "OFF")
         else:
             msg = ""
-            aircoBedroom.log.info("14. AC already off, ignoring turn off action")
+            aircoBedroom.log.debug("14. AC already off, ignoring turn off action")
 
-    # Set and report (new) airco state.
+    #--- Set and report (new) airco state.
     if msg != "":
         if (turnOn and items["AC_Power_Bedroom"] != ON) or (not turnOn and items["AC_Power_Bedroom"] == ON):    #acIsOn != turnOn:
             NotificationAction.sendBroadcastNotification(msg)
             aircoBedroom.log.info(msg)
             if items["AC_MessageRepeat"] != ON:
-                aircoBedroom.log.info("15. Sending message [{}] to Alexa".format(msg))
-                # Echo_TTS_Livingroom.sendCommand(msg)
+                aircoBedroom.log.debug("15. Sending message [{}] to Alexa".format(msg))
+                Echo_TTS_Livingroom.sendCommand(msg)
                 events.sendCommand("AC_MessageRepeat", "ON")
             else:
-                aircoBedroom.log.info("16. Skipping message [{}] to Alexa".format(msg))
+                aircoBedroom.log.debug("16. Skipping message [{}] to Alexa".format(msg))
     else:
-        aircoBedroom.log.info("17. Airco on state is [{}], desired turn on state [{}]".format(items["AC_Power_Bedroom"] == ON, turnOn))
+        aircoBedroom.log.debug("17. Airco on state is [{}], desired turn on state [{}]".format(items["AC_Power_Bedroom"] == ON, turnOn))
